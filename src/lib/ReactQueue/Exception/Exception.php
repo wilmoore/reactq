d66c90975042772ca37c44f6b806f68b6f782e22
Wil Moore III <wil.moore@wilmoore.com>
2011-04-15 01:23:17 -0600

    changed directory names

--- src/lib/ReactQueue/Exception/Exception.php
+++ src/lib/ReactQueue/Exception/Exception.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * @package     reacton
+ * @package     ReactQueue
  * @license     http://www.opensource.org/licenses/mit-license.html
  * @copyright   Full license/copyright information can be found in the LICENSE file distributed with this source code.
  */
 
-namespace reacton;
+namespace ReactQueue\Exception;
 
 /**
- * @package     reacton
+ * @package     ReactQueue
  * @license     http://www.opensource.org/licenses/mit-license.html
  * @copyright   Full license/copyright information can be found in the LICENSE file distributed with this source code.
  */
--- src/lib/ReactQueue/Exception/MethodNotFoundException.php
+++ src/lib/ReactQueue/Exception/MethodNotFoundException.php
@@ -1,16 +1,16 @@
 <?php
 /**
- * @package     reacton
+ * @package     ReactQueue
  * @license     http://www.opensource.org/licenses/mit-license.html
  * @copyright   Full license/copyright information can be found in the LICENSE file distributed with this source code.
  */
 
-namespace reacton\exception;
-      use reacton\Exception,
+namespace ReactQueue\Exception;
+      use ReactQueue\Exception\Exception,
           RuntimeException;
 
 /**
- * @package     reacton
+ * @package     ReactQueue
  * @license     http://www.opensource.org/licenses/mit-license.html
  * @copyright   Full license/copyright information can be found in the LICENSE file distributed with this source code.
  */
