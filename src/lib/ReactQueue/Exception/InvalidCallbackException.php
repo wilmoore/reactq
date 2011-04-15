32edbfd291bfc290bb2823f5e99e54be5450d6ce
Wil Moore III <wil.moore@wilmoore.com>
2011-04-15 00:41:04 -0600

    changed case of exception to Exception

---
+++ src/lib/ReactQueue/Exception/InvalidCallbackException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package     reacton
+ * @license     http://www.opensource.org/licenses/mit-license.html
+ * @copyright   Full license/copyright information can be found in the LICENSE file distributed with this source code.
+ */
+
+namespace reacton\exception;
+      use reacton\Exception,
+          DomainException;
+
+/**
+ * @package     reacton
+ * @license     http://www.opensource.org/licenses/mit-license.html
+ * @copyright   Full license/copyright information can be found in the LICENSE file distributed with this source code.
+ */
+class InvalidCallbackException extends DomainException implements Exception {}
