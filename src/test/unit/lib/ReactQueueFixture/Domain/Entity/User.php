e10a5a8d7de71fc4457803342d77e8d2c78501be
Wil Moore III <wil.moore@wilmoore.com>
2011-04-15 01:41:44 -0600

    moving directories

---
+++ src/test/unit/lib/ReactQueueFixture/Domain/Entity/User.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package     ReactQueue
+ * @license     http://www.opensource.org/licenses/mit-license.html
+ * @copyright   Full license/copyright information can be found in the LICENSE file distributed with this source code.
+ */
+
+namespace ReactQueueFixture\Domain\Entity;
+
+/**
+ * User domain entity (used as context/target object when triggering events)
+ *
+ * @package     ReactQueue
+ * @license     http://www.opensource.org/licenses/mit-license.html
+ * @copyright   Full license/copyright information can be found in the LICENSE file distributed with this source code.
+ */
+class User  {
+    public $name = 'jane';
+}
