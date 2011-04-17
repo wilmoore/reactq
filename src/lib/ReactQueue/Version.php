4eb25019306c5a4f451bb06fe97168a1672209bc
Wil Moore III <wil.moore@wilmoore.com>
2011-04-17 04:10:03 -0600

    added Version.php using 0.0.1-DEV

---
+++ src/lib/ReactQueue/Version.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package     ReactQueue
+ * @license     http://www.opensource.org/licenses/mit-license.html
+ * @copyright   Full license/copyright information can be found in the LICENSE file distributed with this source code.
+ */
+
+namespace ReactQueue;
+
+/**
+ * Class to store and retrieve the current version
+ *
+ * @package     ReactQueue
+ * @license     http://www.opensource.org/licenses/mit-license.html
+ * @copyright   Full license/copyright information can be found in the LICENSE file distributed with this source code.
+ */
+class Version {
+
+    /**
+     * Current Version
+     */
+    const VERSION = '0.0.1-DEV';
+
+}
